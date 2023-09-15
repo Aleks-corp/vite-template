e30e1f19e23869c57fdb312dc4ab18fd9cdf5d4a
Aleks Haran <101347633+Aleks-corp@users.noreply.github.com>
2023-09-15 19:21:19 +0300

    fix404ForGhPages/fixDeploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/vite-template",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        404: resolve("404.html"),
+      },
+    },
+  },
 });
